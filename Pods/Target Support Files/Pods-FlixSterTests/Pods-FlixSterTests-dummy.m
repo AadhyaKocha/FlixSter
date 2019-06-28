4c3b3389510d7a4ee47b07ebedaa4e926a592cb4
Aadhya Kocha <aadhya@fb.com>
2019-06-28 16:03:36 -0700

    new branch

---
+++ Pods/Target Support Files/Pods-FlixSterTests/Pods-FlixSterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixSterTests : NSObject
+@end
+@implementation PodsDummy_Pods_FlixSterTests
+@end
---
+++ Pods/Target Support Files/Pods-FlixSterUITests/Pods-FlixSterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixSterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FlixSterUITests
+@end
